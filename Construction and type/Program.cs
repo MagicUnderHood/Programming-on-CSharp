9aeb7cd199bf0a288f30cea5c5e451bf30635280
Tanya <tanya.orlova17@gmail.com>
2019-04-22 23:39:48 +0300

    Add project files.

---
+++ Construction and type/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Construction_and_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
